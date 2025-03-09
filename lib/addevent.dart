45e8b52e3f0698413827f44483a29d946a4bfd1b
SiddheshM716 <143271532+SiddheshM716@users.noreply.github.com>
2025-03-09 19:27:09 +0530

    Update addevent.dart

--- lib/addevent.dart
+++ lib/addevent.dart
@@ -5,7 +5,6 @@ import 'package:intl/intl.dart';
 import 'dart:async';
 import 'dart:io';
 
-
 class EventFormScreen extends StatefulWidget {
   @override
   _EventFormScreenState createState() => _EventFormScreenState();
@@ -14,7 +13,7 @@ class EventFormScreen extends StatefulWidget {
 class _EventFormScreenState extends State<EventFormScreen> {
   final _formKey = GlobalKey<FormState>();
   final SupabaseClient supabase = Supabase.instance.client;
-  
+
   String? userId;
   String? posterUrl;
 
@@ -23,7 +22,7 @@ class _EventFormScreenState extends State<EventFormScreen> {
   final TextEditingController _categoryController = TextEditingController();
   final TextEditingController _maxTeamSizeController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
-  
+
   DateTime? _startDate;
   DateTime? _endDate;
 
@@ -127,98 +126,131 @@ class _EventFormScreenState extends State<EventFormScreen> {
       );
     }
   }
-Future<double> _getImageDimensions(String imageUrl) async {
-  final Completer<double> completer = Completer();
-  final Image image = Image.network(imageUrl);
-  image.image.resolve(ImageConfiguration()).addListener(
-    ImageStreamListener((ImageInfo info, bool _) {
-      final double aspectRatio = info.image.width / info.image.height;
-      completer.complete(aspectRatio);
-    }),
-  );
-  return completer.future;
-}
+
+  Future<double> _getImageDimensions(String imageUrl) async {
+    final Completer<double> completer = Completer();
+    final Image image = Image.network(imageUrl);
+    image.image.resolve(ImageConfiguration()).addListener(
+      ImageStreamListener((ImageInfo info, bool _) {
+        final double aspectRatio = info.image.width / info.image.height;
+        completer.complete(aspectRatio);
+      }),
+    );
+    return completer.future;
+  }
+
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(title: Text('Create Event')),
-    body: Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Image Picker
-              posterUrl == null
-                  ? ElevatedButton(
-                      onPressed: _pickAndUploadImage,
-                      child: Text('Select Poster'),
-                    )
-                  : FutureBuilder(
-                      future: _getImageDimensions(posterUrl!), // Get image dimensions
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData) {
-                          return CircularProgressIndicator();
-                        }
-                        final double aspectRatio = snapshot.data!;
-                        final double width = 200 * aspectRatio;
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: Colors.blue, // Change the primary color to blue
+        ),
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Colors.blue, // Change the text cursor color to blue
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(title: Text('Create Event')),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Image Picker
+                  GestureDetector(
+                    onTap: _pickAndUploadImage,
+                    child: Container(
+                      width: double.infinity,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.grey),
+                      ),
+                      child: posterUrl == null
+                          ? Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.add_a_photo, size: 40, color: Colors.grey),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    'Add Poster',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          : FutureBuilder(
+                              future: _getImageDimensions(posterUrl!), // Get image dimensions
+                              builder: (context, snapshot) {
+                                if (!snapshot.hasData) {
+                                  return CircularProgressIndicator();
+                                }
+                                final double aspectRatio = snapshot.data!;
+                                final double width = 200 * aspectRatio;
 
-                        return GestureDetector(
-                          onTap: _pickAndUploadImage,
-                          child: Image.network(
-                            posterUrl!,
-                            width: width,
-                            height: 200,
-                            fit: BoxFit.cover,
-                          ),
-                        );
-                      },
+                                return Image.network(
+                                  posterUrl!,
+                                  width: width,
+                                  height: 200,
+                                  fit: BoxFit.cover,
+                                );
+                              },
+                            ),
                     ),
-              SizedBox(height: 20),
+                  ),
+                  SizedBox(height: 20),
 
-              // Event Name & Other Fields
-              _buildTextField(_eventNameController, 'Event Name'),
-              _buildTextField(_venueController, 'Venue'),
-              _buildTextField(_categoryController, 'Category'),
-              _buildTextField(_maxTeamSizeController, 'Max Team Size', keyboardType: TextInputType.number),
-              _buildTextField(_descriptionController, 'Description', maxLines: 3),
+                  // Event Name & Other Fields
+                  _buildTextField(_eventNameController, 'Event Name'),
+                  _buildTextField(_venueController, 'Venue'),
+                  _buildTextField(_categoryController, 'Category'),
+                  _buildTextField(_maxTeamSizeController, 'Max Team Size', keyboardType: TextInputType.number),
+                  _buildTextField(_descriptionController, 'Description', maxLines: 3),
 
-              // Event Schedule Box
-              Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Event Schedule',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  // Event Schedule Box
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                    Divider(color: Colors.grey),
-                    _buildDateTile('Start Date & Time', _startDate, () => _pickDateTime(true)),
-                    _buildDateTile('End Date & Time', _endDate, () => _pickDateTime(false)),
-                  ],
-                ),
-              ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Event Schedule',
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Divider(color: Colors.grey),
+                        _buildDateTile('Start Date & Time', _startDate, () => _pickDateTime(true)),
+                        _buildDateTile('End Date & Time', _endDate, () => _pickDateTime(false)),
+                      ],
+                    ),
+                  ),
 
-              SizedBox(height: 20),
+                  SizedBox(height: 20),
 
-              ElevatedButton(
-                onPressed: _submitEvent,
-                child: Text('Submit Event'),
+                  ElevatedButton(
+                    onPressed: _submitEvent,
+                    child: Text('Submit Event'),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 
   Widget _buildTextField(TextEditingController controller, String label, {TextInputType keyboardType = TextInputType.text, int maxLines = 1}) {
     return Padding(
